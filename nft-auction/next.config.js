bd1f4841f698bbcc719243618d4eec15bbe7d697
makluganteng11 <vincentowent1310@gmail.com>
2022-11-17 20:31:18 +0800

    fix

--- nft-auction/next.config.js
+++ nft-auction/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    outputStandalone: true,
+  },   
+  images: {
+    "loader": "akamai",
+    "path": ""
+  }
 }
 module.exports = {
   trailingSlash: true,
