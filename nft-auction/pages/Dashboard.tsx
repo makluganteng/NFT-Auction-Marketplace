a53f1467b7e52925893fbd526fac7077c291ed4c
makluganteng <makluganteng11@gmail.com>
2022-11-19 23:24:54 +0800

    Header added

---
+++ nft-auction/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Header from "./components/header";
+
+const Dashboard: NextPage = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default Dashboard;
