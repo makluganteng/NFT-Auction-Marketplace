7d007e0902e19cab83f23a5857da1061d4738874
0xkeivin <keivincatwork@gmail.com>
2022-11-22 00:30:10 +0545

    removed Route causing build error

--- nft-auction/pages/components/myNft.tsx
+++ nft-auction/pages/components/myNft.tsx
@@ -1,50 +1,53 @@
-import { NextPage } from "next"
-import { useEffect, useState } from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import styled from "styled-components"
-import Card from "./card"
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import styled from "styled-components";
+import Card from "./card";
 
 const MainContainer = styled.div`
     background-color: black
     color: white;
     display: flex;
     
-`
+`;
 
-const CardContainer = styled.div`
-
-`
+const CardContainer = styled.div``;
 
 export type NFT = {
-    name: string
-    price: string
-}
+  name: string;
+  price: string;
+};
 
 const MyNft = () => {
+  const [nft, setNft] = useState<Array<NFT>>();
 
-    const[nft,setNft] = useState<Array<NFT>>()
+  useEffect(() => {
+    setNft([
+      { name: "LALA", price: "" },
+      { name: "Stark", price: "" },
+      { name: "heyoNFT", price: "" },
+    ]);
+    console.log(nft);
+  }, [nft]);
 
-    useEffect(()=>{
-        setNft([{name: "LALA",price: ""},{name: "Stark", price: ""},{name:"heyoNFT", price: ""}])
-        console.log(nft)
-    },[])
+  return (
+    <>
+      <MainContainer>
+        {
+             nft ? nft.map((value: NFT, key)=>{
+                  return(
+                      <CardContainer key={key}>
+                              <Card name={value.name} price={value.price}/>
 
-    return(
-        <Route>
-        <MainContainer>
-            {
-               nft ? nft.map((value: NFT, key)=>{
-                    return(
-                        <CardContainer key={key}>
-                                <Card name={value.name} price={value.price}/>
-                        </CardContainer>
-                        
-                    )
-                }) : <></>
-            }
-        </MainContainer>
-        </Route>
-    )
-}
+                      </CardContainer>
 
-export default MyNft
+                  )
+              }) : <></>
+          
+        }
+      </MainContainer>
+    </>
+  );
+};
+
+export default MyNft;
