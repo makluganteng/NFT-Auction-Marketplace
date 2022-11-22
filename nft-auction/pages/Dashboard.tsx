156b425c41344ebdb2c8b6e6a9de305adbae2ef0
0xkeivin <keivincatwork@gmail.com>
2022-11-22 23:24:54 +0545

    refactored props for mainbar

--- nft-auction/pages/Dashboard.tsx
+++ nft-auction/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import Header from "./components/header";
 import MainBar from "./components/mainbar";
 import SideBar from "./components/sidebar";
 import styled from "styled-components";
+import MyNft from "./components/myNft";
 
 
 const Container = styled.div`
@@ -15,7 +16,7 @@ const Dashboard: NextPage = () => {
             <Header />
             <Container>
                 <SideBar />
-                <MainBar />
+                <MainBar children={<MyNft/>}/>
             </Container>
         </>
     )
