be6fd56cd58f0184d290d00183c51e85969e9bc2
0xkeivin <keivincatwork@gmail.com>
2022-11-24 23:54:46 +0700

    added login modal

--- nft-auction/pages/index.tsx
+++ nft-auction/pages/index.tsx
@@ -1,10 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Header from "./components/header";
+import MainBar from "./components/mainbar";
+import styled from "styled-components";
+import LoginModal from "./components/loginModal";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 export default function Home() {
   return (
     <>
+      <Header />
+      <Container>
+        <MainBar>
+          <LoginModal />
+        </MainBar>
+      </Container>
     </>
-  )
+  );
 }
