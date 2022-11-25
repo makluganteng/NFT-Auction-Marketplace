2e770104eccc058f7e9523bb2c5d66de74bb3182
0xkeivin <keivincatwork@gmail.com>
2022-11-25 15:14:05 +0700

    added any type

--- nft-auction/pages/api/auction/[nftid].ts
+++ nft-auction/pages/api/auction/[nftid].ts
@@ -1,21 +1,20 @@
 import axios from "axios"
 
-export default async (req, res) => {
-    if (req.method === 'GET') {
-      const id = req.query
-      try {
-        const StatusResponse = await axios.get(`http://localhost:3030/auction/isListed/${id}`)
-        if (StatusResponse && StatusResponse.data) {
-          res.statusCode = 200
-          res.json({ ...StatusResponse.data })
-        } else {
-          res.statusCode = 500
-          res.json({ error: 'Something went wrong' })
-        }
-      } catch (error) {
+export default async (req: any, res: any) => {
+  if (req.method === 'GET') {
+    const id = req.query
+    try {
+      const StatusResponse = await axios.get(`http://localhost:3030/auction/isListed/${id}`)
+      if (StatusResponse && StatusResponse.data) {
+        res.statusCode = 200
+        res.json({ ...StatusResponse.data })
+      } else {
         res.statusCode = 500
-        res.json({ error })
+        res.json({ error: 'Something went wrong' })
       }
+    } catch (error) {
+      res.statusCode = 500
+      res.json({ error })
     }
   }
-  
+}
