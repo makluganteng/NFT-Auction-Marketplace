e58559696e62c7ad6f8fab6cdb2a2d689365fd3c
0xkeivin <keivincatwork@gmail.com>
2022-11-25 15:13:51 +0700

    added any type

--- nft-auction/pages/api/auction/index.ts
+++ nft-auction/pages/api/auction/index.ts
@@ -16,7 +16,7 @@ import axios from 'axios'
 //         const createResponse = await axios.get(
 //           `http://34.126.88.235:3030/auction/isListed`,
 //         )
-  
+
 //         if (createResponse && createResponse.data) {
 //           res.statusCode = 200
 //           res.json({ ...createResponse.data })
@@ -27,7 +27,7 @@ import axios from 'axios'
 //       }
 //      } catch (error) {
 //         res.statusCode = 500
-  
+
 //         if (error.response.status === 403) {
 //           res.statusCode = 403
 //         }
@@ -36,55 +36,55 @@ import axios from 'axios'
 //     }
 //   }
 
-export default async (req,res) =>{
-    if (req.method === 'POST') {
-        const tokenId = req.body.tokenid
-        const startingPrice = req.body.startingPrice
-        const hours = req.body.hours
-      try {
-        const createResponse = await axios.post(`http://34.126.88.235:3030/auction/list`,{
-                token_id: tokenId,
-                startingPrice: startingPrice,
-                hours: hours
-            })
-  
-        if (createResponse && createResponse.data) {
-          res.statusCode = 200
-          res.json({ ...createResponse.data })
-        } else {
-          res.statusCode = 500
-          res.json({ error: 'Something went wrong' })
-        }
-      } catch (error) {
-        res.statusCode = 500
-  
-        if (error.response.status === 403) {
-          res.statusCode = 403
-        }
-        res.json({ error })
-      }
-    }
+export default async (req: any, res: any) => {
+  if (req.method === 'POST') {
+    const tokenId = req.body.tokenid
+    const startingPrice = req.body.startingPrice
+    const hours = req.body.hours
+    try {
+      const createResponse = await axios.post(`http://34.126.88.235:3030/auction/list`, {
+        token_id: tokenId,
+        startingPrice: startingPrice,
+        hours: hours
+      })
 
-    if (req.method === 'GET') {
-      try {
-        const createResponse = await axios.get(`http://34.126.88.235:3030/auction/isListed`)
-  
-        if (createResponse && createResponse.data) {
-          res.statusCode = 200
-          res.json({ ...createResponse.data })
-        } else {
-          res.statusCode = 500
-          res.json({ error: 'Something went wrong' })
-        }
-      } catch (error) {
+      if (createResponse && createResponse.data) {
+        res.statusCode = 200
+        res.json({ ...createResponse.data })
+      } else {
         res.statusCode = 500
-  
-        if (error.response.status === 403) {
-          res.statusCode = 403
-        }
-        res.json({ error })
+        res.json({ error: 'Something went wrong' })
       }
+    } catch (error: any) {
+      res.statusCode = 500
+
+      if (error.response.status === 403) {
+        res.statusCode = 403
+      }
+      res.json({ error })
     }
-    
+  }
+
+  if (req.method === 'GET') {
+    try {
+      const createResponse = await axios.get(`http://34.126.88.235:3030/auction/isListed`)
+
+      if (createResponse && createResponse.data) {
+        res.statusCode = 200
+        res.json({ ...createResponse.data })
+      } else {
+        res.statusCode = 500
+        res.json({ error: 'Something went wrong' })
+      }
+    } catch (error: any) {
+      res.statusCode = 500
+
+      if (error.response.status === 403) {
+        res.statusCode = 403
+      }
+      res.json({ error })
+    }
+  }
+
 
 }
